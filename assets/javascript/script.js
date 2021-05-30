ac3bc0bd925c7bdc0e8b3008ea0ed809b67843f4
Mattmcds <82618353+Mattmcds@users.noreply.github.com>
2021-05-30 16:38:47 -0600

    date added to top

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+//Display current day at the top of the page
+
+var day = new Date();
+document.getElementById("currentDay").innerHTML = day;
+
+
+
+
+// have current time change colors of background
+
+//Add text to boxes
+
+$(".to-do-box").on("click", "p", function() {
+
+});
+
+//button saves and saves in local storage
