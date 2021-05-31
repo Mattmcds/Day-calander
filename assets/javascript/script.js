aa5e9998845d51746d30e0e9353e51183927e8e9
Mattmcds <82618353+Mattmcds@users.noreply.github.com>
2021-05-30 20:41:46 -0600

    attempted js for color change and save button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,16 +2,47 @@
 
 var day = new Date();
 document.getElementById("currentDay").innerHTML = day;
+console.log(day)
 
+var notes=$('#to-do-box').val();
+
+var saveTasks = function(){
+    localStorage.setItem("notes", JSON.stringify(notes));
+
+   // if (!notes) {
+      //  notes =
+   // }
+};
+
+
+
+    
+$(document).ready(function(){
+    var saveBtn = document.getElementById("save");
+
+
+})
 
 
 
 // have current time change colors of background
 
+var hours = day.getHours();
+    console.log(hours)
+
+hours = function(){
+    if()
+}
+
 //Add text to boxes
 
-$(".to-do-box").on("click", "p", function() {
-
+$(".to-do-box").on("click", "textarea", function() {
+    var notes = $(this).text();
+    console.log(text);
 });
 
 //button saves and saves in local storage
+
+
+
+// if( x < 10 && x > 9)
